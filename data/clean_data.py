0a35f7695fb5b1e39317553fb91c5533165b99d0
Maum <mauricio.marin.201@gmail.com>
2023-05-13 15:00:15 -0600

    Fixed csv errors

--- data/clean_data.py
+++ data/clean_data.py
@@ -4,16 +4,17 @@ from os.path import isfile, join
 
 
 def transform_datasets():
-    raw_gdp_path = 'raw_gdp_datasets'
-    file_names = [f for f in listdir(raw_gdp_path) if isfile(join(raw_gdp_path, f))]
+    path = 'raw_datasets'
+    file_names = [f for f in listdir(path) if isfile(join(path, f))]
 
     for file in file_names:
-        df = pd.read_csv(f"data/raw_gdp_datasets/{file}")
+        df = pd.read_csv(f"{path}/{file}")
         year_cols = list(df.columns[2:])
 
         df = pd.melt(df, id_vars=['Country Name', 'Code'], var_name=['Year'], value_vars=year_cols)
         df = df.sort_values(by=['Country Name', 'Year'])
-        df.to_csv(f'final_datasets/{file}')
+        df = df.dropna(subset='value')
+        df.to_csv(f'final_datasets/{file}', index=0)
 
 
 transform_datasets()
