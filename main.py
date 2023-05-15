4d0065352a231c07c1935210ef583e469c43171c
jcordero26 <javiercordero1296@gmail.com>
2023-05-14 22:54:47 -0600

    Adding slider for year selection

--- main.py
+++ main.py
@@ -19,9 +19,10 @@ st.set_page_config(
     page_icon = '📈',
     layout = 'wide'
 )
+global_year=2020
 
-def apply_filter(year):
-    df = utility.get_gdp_by_year("gdp",year)
+def apply_filter(metric,year):
+    df = utility.get_gdp_by_year(metric,year)
     df2 = df["dataframe"]
     print(df2)
     geo_df_tmp = gpd.GeoDataFrame.from_features(
@@ -29,8 +30,6 @@ def apply_filter(year):
     ).rename(columns={"ISO_A3":"Code"})
 
     geo_df = geo_df_tmp.merge(df2, how="inner").set_index("Code")
-    #print(geo_df)
-    #).merge(df2, on="Code").set_index("ISO_A3")
 
     fig = px.choropleth_mapbox(geo_df,
                                geojson=geo_df.geometry,
@@ -48,22 +47,10 @@ with open('styles.css') as f:
 
 with st.sidebar:
 
-    with st.form('filters'):
-        PROVINCES = ['Cartago', 'San Jose', 'Guanacaste', 'Heredia', 'Alajuela', 'Limon', 'Puntarenas', 'Unknown']
+    global_year = st.slider('Year', 1960, 2021, 2020)
 
-        with st.expander('', expanded = True):
-            year = selectbox("Year", options = [2019, 2020, 2021], label_visibility = 'visible', no_selection_label = 'All')
-        with st.expander('', expanded = True):
-            province = selectbox("Province", options = PROVINCES, label_visibility = 'visible', no_selection_label = 'All')
-        with st.expander('', expanded = True):
-            time_day = selectbox("Time of day", options = ['Early Morning', 'Morning', 'Night', 'Afternoon'], label_visibility = 'visible', no_selection_label = 'All')
-        with st.expander('', expanded = True):
-            gender = selectbox("Gender", options = ['Male', 'Female', 'Non applicable'], label_visibility = 'visible', no_selection_label = 'All')
-
-        st.form_submit_button('Apply    ', type = 'primary', use_container_width= True)
-
-tab1, tab2 = st.tabs(["Map1", "Map2"])
+tab1, tab2 = st.tabs(["PIB per capita", "Casos de crecimiento"])
 
 with tab1:
-   fig = apply_filter(2020)
+   fig = apply_filter("gdp_per_capita",global_year)
    st.plotly_chart(fig, use_container_width=True)
