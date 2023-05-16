2945dd4ca094cc8caef632188c1f71e42f3623c4
Maum <mauricio.marin.201@gmail.com>
2023-05-16 00:37:40 -0600

    Tab 3

--- main.py
+++ main.py
@@ -14,6 +14,8 @@ geodata = json.load(f)
 
 utility = utils.Utils()
 
+values = ['gdp','gdp_growth','gdp_per_capita_growth','gdp_per_capita','gdp_ppp','gdp_ppp_per_capita']
+
 st.set_page_config(
     page_title = 'GDP/Employment Dashboard',
     page_icon = '📈',
@@ -24,23 +26,26 @@ global_year=2020
 def apply_filter(metric, year):
     df = utility.get_gdp_by_year(metric, year)
     df2 = df["dataframe"]
-    print(df['min'], df['max'])
     geo_df_tmp = gpd.GeoDataFrame.from_features(
         geodata["features"]
     ).rename(columns={"ISO_A3":"Code"})
 
     geo_df = geo_df_tmp.merge(df2, how="inner").set_index("Code")
 
-    fig = px.choropleth_mapbox(geo_df,
-                               geojson=geo_df.geometry,
-                               locations=geo_df.index,
-                               color='value',
-                               color_continuous_scale="haline",
-                               center={"lat": 0, "lon": 0},
-                               mapbox_style="carto-positron",
-                               zoom=1.2,
-                               range_color=[df['min'], df['max']], #
-                               height=1100)
+    fig = px.choropleth_mapbox(
+        geo_df,
+        geojson=geo_df.geometry,
+        locations=geo_df.index,
+        color='value',
+        color_continuous_scale="haline",
+        center={"lat": 0, "lon": 0},
+        mapbox_style="carto-positron",
+        zoom=1.2,
+        hover_data=['Country Name', 'value'],
+        range_color=[df['min'], df['max']],
+        height=1100
+    )
+
     return fig
 
 #load custom css
@@ -48,11 +53,37 @@ with open('styles.css') as f:
     st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html = True)
 
 with st.sidebar:
+    global_year = st.slider('Year', 1960, 2020, 2010)
 
-    global_year = st.slider('Year', 1960, 2021, 2010)
-
-tab1, tab2 = st.tabs(["PIB per capita", "Casos de crecimiento"])
+tab1, tab2, tab3 = st.tabs(["PIB per capita", "Casos de crecimiento", "Ver datos de país"])
 
 with tab1:
-   fig = apply_filter("gdp_per_capita",global_year)
-   st.plotly_chart(fig, use_container_width=True)
+	with st.container():
+		fig = apply_filter("gdp_per_capita",global_year)
+		st.plotly_chart(fig, use_container_width=True)
+		
+
+
+with tab3:
+	with st.container():
+		col1, col2, col3 = st.columns(3)
+
+		charts = []
+		country = 'Costa Rica'
+
+		for i in values:
+			t = utility.get_gdp_by_country(i, country)
+			chart = px.scatter(t['df'], x = 'Year', y = 'value', title=i, labels=['year', i])
+			charts.append(chart)
+
+
+		with col1:
+			st.plotly_chart(charts[0], use_container_width=True)
+			st.plotly_chart(charts[3], use_container_width=True)
+		with col2:
+			st.plotly_chart(charts[1], use_container_width=True)
+			st.plotly_chart(charts[4], use_container_width=True)
+		with col3:
+			st.plotly_chart(charts[2], use_container_width=True)
+			st.plotly_chart(charts[5], use_container_width=True)
+
--- utils/fetch_data.py
+++ utils/fetch_data.py
@@ -2,33 +2,26 @@ import pandas as pd
 
 class Utils:
 
+    path = 'data/final_datasets'
+
     def __init__(self) -> None:
-        path = 'data/final_datasets'
-        
-        self.gdp = pd.read_csv(f"{path}/gdp.csv", quotechar='"')
-        self.gdp_growth = pd.read_csv(f"{path}/gdp_growth.csv")
-        self.gdp_per_capita_growth = pd.read_csv(f"{path}/gdp_per_capita_growth.csv")
-        self.gdp_per_capita = pd.read_csv(f"{path}/gdp_per_capita.csv")
-        self.gdp_ppp = pd.read_csv(f"{path}/gdp_ppp.csv")
-        self.gdp_ppp_per_capita = pd.read_csv(f"{path}/gdp_ppp_per_capita.csv")
-        self.unemployment = pd.read_csv(f"{path}/unemployment analysis.csv")
-        self.happiness = pd.read_csv(f"{path}/Happiness_data.csv")
+        pass
 
     def get_gdp_by_year(self, val, year):
         temp = ''
 
         if val == 'gdp':
-            temp = self.gdp
+            temp = pd.read_csv(f"{self.path}/gdp.csv", quotechar='"')  
         elif val == 'gdp_growth':
-            temp = self.gdp_growth
+            temp = pd.read_csv(f"{self.path}/gdp_growth.csv")
         elif val == 'gdp_per_capita_growth':
-            temp = self.gdp_per_capita_growth
+            temp = pd.read_csv(f"{self.path}/gdp_per_capita_growth.csv")
         elif val == 'gdp_per_capita':
-            temp = self.gdp_per_capita
+            temp = pd.read_csv(f"{self.path}/gdp_per_capita.csv")
         elif val == 'gdp_ppp':
-            temp = self.gdp_ppp
+            temp = pd.read_csv(f"{self.path}/gdp_ppp.csv")
         elif val == 'gdp_ppp_per_capita':
-            temp = self.gdp_ppp_per_capita
+            temp = pd.read_csv(f"{self.path}/gdp_ppp_per_capita.csv")
         
         temp = temp[ temp['Year'] == year ]
 
@@ -51,32 +44,42 @@ class Utils:
         temp = ''
 
         if val == 'gdp':
-            temp = self.gdp
+            temp = pd.read_csv(f"{self.path}/gdp.csv", quotechar='"')  
         elif val == 'gdp_growth':
-            temp = self.gdp_growth
+            temp = pd.read_csv(f"{self.path}/gdp_growth.csv")
         elif val == 'gdp_per_capita_growth':
-            temp = self.gdp_per_capita_growth
+            temp = pd.read_csv(f"{self.path}/gdp_per_capita_growth.csv")
         elif val == 'gdp_per_capita':
-            temp = self.gdp_per_capita
+            temp = pd.read_csv(f"{self.path}/gdp_per_capita.csv")
         elif val == 'gdp_ppp':
-            temp = self.gdp_ppp
+            temp = pd.read_csv(f"{self.path}/gdp_ppp.csv")
         elif val == 'gdp_ppp_per_capita':
-            temp = self.gdp_ppp_per_capita
+            temp = pd.read_csv(f"{self.path}/gdp_ppp_per_capita.csv")
         
         temp = temp[ temp['Country Name'] == country ]
+
+        ret_dict = {
+            'df': temp,
+            'years': temp['Year'],
+            'values': temp['value']
+        }
         
-        return temp
+        return ret_dict
     
     def get_unemployment_by_year(self, year):
-        return self.unemployment[ self.unemployment['Year'] == year ]
+        temp = pd.read_csv(f"{self.path}/unemployment analysis.csv")
+        return temp[ temp['Year'] == year ]
     
     def get_unemployment_by_country(self, country):
-        return self.unemployment[ self.unemployment['Country Name'] == country ]
+        temp = pd.read_csv(f"{self.path}/unemployment analysis.csv")
+        return temp[ temp['Country Name'] == country ]
     
     def get_happiness_by_year(self, year):
-        return self.happiness[ self.happiness['year'] == year ]
+        temp = pd.read_csv(f"{self.path}/Happiness_data.csv")
+        return temp[ temp['year'] == year ]
     
     def get_happiness_by_country(self, country):
-        return self.happiness[ self.happiness['Country name'] == country ]
+        temp = pd.read_csv(f"{self.path}/Happiness_data.csv")
+        return temp[ temp['Country name'] == country ]
     
 # utils = Utils()
